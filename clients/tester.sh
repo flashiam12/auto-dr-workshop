cdbfc02e6df3ac10f00f54a1435a1c6b4b49cb83
Ubuntu <ubuntu@ip-10-0-64-17.us-east-2.compute.internal>
2024-10-09 12:56:08 +0000

    initial

--- clients/tester.sh
+++ clients/tester.sh
@@ -4,18 +4,18 @@
 export KAFKA_TOPIC="auto-dr"
 export MIRROR_KAFKA_TOPIC="west-auto-dr"
 export KAFKA_CLUSTER_ENV="primary"
-export KAFKA_BOOTSTRAP="pkc-k85ygg.us-east-1.aws.confluent.cloud:9092"
-export KAFKA_USERNAME="QCUNHZTW325QNHSK"
-export KAFKA_PASSWORD="KobCn4/5wr6w/fZAT2jUs1sqsL50f1oMJZl4nCYlNAzwfjnI6QzdtgM4iMqdDy6B"
+export KAFKA_BOOTSTRAP="pkc-xxxx.us-east-1.aws.confluent.cloud:9092"
+export KAFKA_USERNAME="QCUNHZTWXXXXXXXX"
+export KAFKA_PASSWORD="KobCn4/5wr6w/fZAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 
 # Secondary  
 # export KAFKA_TOPIC="auto-dr"
 # export MIRROR_KAFKA_TOPIC="east-auto-dr"
 # export KAFKA_CLUSTER_ENV="secondary"
-# export KAFKA_BOOTSTRAP="pkc-pg707m.us-west-2.aws.confluent.cloud:9092"
-# export KAFKA_USERNAME="NUCYUTUEHINK67VM"
-# export KAFKA_PASSWORD="c8soD1XmGke6UyiJmvXG4vPE1elFlRFve8hO0RQwSlIFpWo4jovgwxXbpsl45V5w"
+# export KAFKA_BOOTSTRAP="pkc-xxxx.us-west-2.aws.confluent.cloud:9092"
+# export KAFKA_USERNAME="NUCYUTXXXXXXXXXX"
+# export KAFKA_PASSWORD="c8soD1XmGkeXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 source ../.venv/bin/activate
 
--- monitoring.tf
+++ monitoring.tf
@@ -1,43 +1,43 @@
-# resource "kubernetes_namespace" "monitoring" {
-#   provider = kubernetes.kubernetes-raw
-#   metadata {
-#     name = "monitoring"
-#     annotations = {
-#       "owner" = "local"
-#       "purpose" = "dr-test"
-#     }
-#   }
-# }
+resource "kubernetes_namespace" "monitoring" {
+  provider = kubernetes.kubernetes-raw
+  metadata {
+    name = "monitoring"
+    annotations = {
+      "owner" = "local"
+      "purpose" = "dr-test"
+    }
+  }
+}
 
-# resource "helm_release" "prometheus" {
-#   name       = "prometheus"
-#   chart      = "${path.module}/dependencies/prometheus"
-#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
-#   cleanup_on_fail = true
-#   create_namespace = false
-#   values = [templatefile("${path.module}/dependencies/prometheus/values.yaml", 
-#             {
-#                 "cloud_api_key" = var.cc_cloud_api_key
-#                 "cloud_api_secret" = var.cc_cloud_api_secret
-#                 "primary_cluster_id" = confluent_kafka_cluster.primary.id
-#                 "secondary_cluster_id" = confluent_kafka_cluster.secondary.id
-#             }
-#             )]
-# }
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  chart      = "${path.module}/dependencies/prometheus"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  cleanup_on_fail = true
+  create_namespace = false
+  values = [templatefile("${path.module}/dependencies/prometheus/values.yaml", 
+            {
+                "cloud_api_key" = var.cc_cloud_api_key
+                "cloud_api_secret" = var.cc_cloud_api_secret
+                "primary_cluster_id" = confluent_kafka_cluster.primary.id
+                "secondary_cluster_id" = confluent_kafka_cluster.secondary.id
+            }
+            )]
+}
 
-# resource "helm_release" "grafana" {
-#   name       = "grafana"
-#   chart      = "${path.module}/dependencies/grafana"
-#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
-#   cleanup_on_fail = true
-#   create_namespace = false
-#   values = [file("${path.module}/dependencies/grafana/values.yaml")]
-#   set {
-#     name = "adminUser"
-#     value = "admin"
-#   }
-#   set {
-#     name = "adminPassword"
-#     value = "confluent"
-#   }
-# }
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  chart      = "${path.module}/dependencies/grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  cleanup_on_fail = true
+  create_namespace = false
+  values = [file("${path.module}/dependencies/grafana/values.yaml")]
+  set {
+    name = "adminUser"
+    value = "admin"
+  }
+  set {
+    name = "adminPassword"
+    value = "confluent"
+  }
+}
